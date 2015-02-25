c3d376afa047cb268a537ab399c8a361bf647274
Sergey Shilin <sergey.shilin@phystech.edu>
2015-02-25 04:19:39 +0300

    prototype is ready

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+	session_start();
+
+	$page = "";
+	$_SESSION['end'] = "Bishop_of_Tasmania";
+	$_SESSION['start'] = "Alan_Turing";
+	$_SESSION['win'] = false;
+
+	if(isset($_GET['page']) && !empty($_GET['page'])) {
+		$page = $_GET['page'];
+		$_SESSION['previous'] = $_SESSION['current'];
+		$_SESSION['current'] = $page;
+		if ($_SESSION['previous'] != $_SESSION['current'])
+			$_SESSION['counter'] += 1;
+		if ($_SESSION['current'] == $_SESSION['start'])
+			$_SESSION['counter'] = 0;
+		if($_SESSION['current'] == $_SESSION['end'])
+			$_SESSION['win'] = true;
+	} else {
+		$_SESSION['counter'] = 0;
+	}
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -18,26 +40,47 @@
 			margin: 0 !important;
 			border: 0 !important;
 		}
+
+		#page_header {
+			width: 100%;
+			height: 50px;
+			border-bottom: 1px solid #a7d7f9;
+		}
 	</style>
 	<script type="text/javascript" language="JavaScript" src="js/jquery.min.js"></script>
 	<script src="js/main.js" type="text/javascript"></script>
 </head>
 <body>
+	<div id="page_header">
+		<?php 
+			if(isset($_GET['page']) && !empty($_GET['page'])) 
+				echo "Текущий счет: ".$_SESSION['counter']."<br>Конечная цель: ". $_SESSION['end'];
+			else 
+				echo "<a href='?page=Alan_Turing'>Старт</a>";
+		?>
+	</div>
 	<?php 
 		include_once('simple_html_dom.php');
+ 
+		if(!$_SESSION['win']) {
+			$url = "http://en.wikipedia.org/wiki/".$page;
+			$html = file_get_html($url);
+			// $html = file_get_html('http://en.wikipedia.org/wiki/Alan_Turing');
+			foreach($html->find('link') as $element) { //выборка всех тегов img на странице
+			       echo $element; // построчный вывод содержания всех найденных атрибутов src
+			}
+			foreach($html->find('script') as $element) { //выборка всех тегов img на странице
+			       echo $element; // построчный вывод содержания всех найденных атрибутов src
+			}
 
-		$html = file_get_html('http://en.wikipedia.org/wiki/Alan_Turing');
-		foreach($html->find('link') as $element) { //выборка всех тегов img на странице
-		       echo $element; // построчный вывод содержания всех найденных атрибутов src
+			//$html->find('div[id=mw-page-base]', 0)->class = 'hidden';
+			$html->find('div[id=content]', 0)->class = 'mw-body zeromargin';
+			$content = $html->find('div[id=content]', 0);
+			echo $content;
+		} else {
+			echo "<h1>Вы выиграли! Ваш счет ". $_SESSION['counter']." очков</h1><br>";
+			echo "<h2><a href='?page=Alan_Turing'>Начать сначала?</a></h2>";
 		}
-		foreach($html->find('script') as $element) { //выборка всех тегов img на странице
-		       echo $element; // построчный вывод содержания всех найденных атрибутов src
-		}
-
-		//$html->find('div[id=mw-page-base]', 0)->class = 'hidden';
-		$html->find('div[id=content]', 0)->class = 'mw-body zeromargin';
-		$content = $html->find('div[id=content]', 0);
-		echo $content;
 	?>	
 </body>
 </html>
