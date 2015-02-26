427c3782a116dab0f6a27c766d4cbdb875a058d8
Sergey Shilin <sergey.shilin@phystech.edu>
2015-02-26 17:42:40 +0300

    title on index was changed

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
 
-    <title>Cover Template for Bootstrap</title>
+    <title>WikiGame | Get it shorter</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
