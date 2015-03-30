d28425e71051895213eab2fdebde8c522b2d860e
Sergey Shilin <sergey.shilin@phystech.edu>
2015-03-30 15:49:00 +0300

    programming unsorted was added

--- filetodatabase.php
+++ filetodatabase.php
@@ -3,6 +3,6 @@ header('Content-Type: text/html; charset=utf-8');
 include 'authorize.php';
 require_once('wikigame/WayParser.php');
 
-$parser = new WayParser('wikigame/scripts/results/tecnic_unsorted.txt');
+$parser = new WayParser('wikigame/scripts/results/programming_unsorted.txt');
 $parser->setLang('ru');
-$parser->writeWays(6);
+$parser->writeWays(7);
