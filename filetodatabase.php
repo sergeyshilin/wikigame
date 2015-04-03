fc5923de01124ce26b6d628e14b8c06b3772c6e9
Sergey Shilin <sergey.shilin@phystech.edu>
2015-04-03 18:42:21 +0300

    some change

--- filetodatabase.php
+++ filetodatabase.php
@@ -3,6 +3,6 @@ header('Content-Type: text/html; charset=utf-8');
 include 'authorize.php';
 require_once('wikigame/WayParser.php');
 
-$parser = new WayParser('wikigame/scripts/results/1_april_route.txt');
+$parser = new WayParser('wikigame/scripts/results/1.txt');
 $parser->setLang('ru');
-$parser->writeWays(3);
+$parser->writeWays(5);
