a9408444a9c337b6818600ed63b9af30df352635
Vladimir Ivashkin <illusionww@gmail.com>
2015-04-06 03:14:37 +0300

    more careful cutting of categories

--- w/classes/PageResolver.php
+++ w/classes/PageResolver.php
@@ -29,7 +29,7 @@ class PageResolver {
         $url = "https://" . $_SESSION['lang'] . ".wikipedia.org/w/index.php?title=" . $name;
         $html = file_get_html($url);
 
-        foreach($html->find('a[class=external text]') as $element) {
+        foreach ($html->find('a[class=external text]') as $element) {
             $link = $element->href;
             $element->href = str_replace("//" . $_SESSION['lang'] . ".wikipedia.org", "", $link);
         }
@@ -45,7 +45,7 @@ class PageResolver {
     }
 
     public function isGenerated($title) {
-        return strpos($title,'Категория:') !== false;
+        return strpos($title, 'Категория:') !== false;
     }
 
     public function extractRedirectPageName($content) {
@@ -57,39 +57,41 @@ class PageResolver {
 
     private function cutFooter($content) {
         $lastPos = 0;
-        $needle = '<h2><span class="mw-headline" id="';
+        $needle = "<h2><span class=\"mw-headline\" id=\"";
 
-        while (($lastPos = strpos($content, $needle, $lastPos))!== false) {
+        while (($lastPos = strpos($content, $needle, $lastPos)) !== false) {
             $end = strpos($content, '">', $lastPos + strlen($needle));
             $str = substr($content, $lastPos + strlen($needle), $end - $lastPos - strlen($needle));
-            if (strcmp($str, ".D0.9F.D1.80.D0.B8.D0.BC.D0.B5.D1.87.D0.B0.D0.BD.D0.B8.D1.8F") == 0 //Примечания
-                || strcmp($str, ".D0.A1.D0.BC._.D1.82.D0.B0.D0.BA.D0.B6.D0.B5") == 0 // См. также
-                || strcmp($str, ".D0.9B.D0.B8.D1.82.D0.B5.D1.80.D0.B0.D1.82.D1.83.D1.80.D0.B0") == 0 // Литература
-                || strcmp($str, ".D0.A1.D1.81.D1.8B.D0.BB.D0.BA.D0.B8") == 0) { // Ссылки
-                $cut_pos = $lastPos;
-                break;
-            }
+            $headers[] = array('pos' => $lastPos, 'str' => $str);
             $lastPos = $lastPos + strlen($needle);
         }
 
-        if (isset($cut_pos) && $cut_pos > 0) {
-            return substr($content, 0, $cut_pos);
-        } else {
-            return $content;
+        $needle = "<table class=\"navbox";
+        $lastPos = strpos($content, $needle, $lastPos);
+        $headers[] = $lastPos != false ? array('pos' => $lastPos, 'str' => "") : array('pos' => strlen($content), 'str' => "");
+
+        $contentNew = $content;
+        foreach ($headers as $key => $header) {
+            $str = $header['str'];
+
+            if (strcmp($str, ".D0.9F.D1.80.D0.B8.D0.BC.D0.B5.D1.87.D0.B0.D0.BD.D0.B8.D1.8F") == 0 //Примечания
+                || strcmp($str, ".D0.9B.D0.B8.D1.82.D0.B5.D1.80.D0.B0.D1.82.D1.83.D1.80.D0.B0") == 0 // Литература
+                || strcmp($str, ".D0.A1.D1.81.D1.8B.D0.BB.D0.BA.D0.B8") == 0
+            ) { // Ссылки
+                $contentNew = str_replace(substr($content, $header['pos'], $headers[$key + 1]['pos'] - $header['pos']), "", $content);
+                break;
+            }
         }
+        return $contentNew;
     }
 
-    public function printPage($title, $content, $redirect = NULL, $redirecttitle = NULL) {
-        $text = '<div id="content" class="mw-body zeromargin" role="main">' .
-         '<h1 id="firstHeading" class="firstHeading" lang="ru">' . $title . '</h1>' .
-         '<div id="bodyContent" class="mw-body-content">' .
-         '<div id="siteSub">Материал из Википедии — свободной энциклопедии</div>';
-        if (isset($redirect) && !empty($redirect) && isset($redirecttitle) && !empty($redirecttitle)) {
-            $text = $text . '<span class="mw-redirectedfrom">(перенаправлено с «<a href="/wiki/' . $redirect . '" title="' . $redirecttitle . '">' . $redirecttitle . '</a>»)</span>';
-        }
-        $text = $text . '<div id="mw-content-text" lang="ru" dir="ltr" class="mw-content-ltr">' .
-         $this->cutFooter($content) .
-         "</div></div>";
-        return $text;
+    public function printPage($title, $content) {
+        return '<div id="content" class="mw-body zeromargin" role="main">' .
+        '<h1 id="firstHeading" class="firstHeading" lang="ru">' . $title . '</h1>' .
+        '<div id="bodyContent" class="mw-body-content">' .
+        '<div id="siteSub">Материал из Википедии — свободной энциклопедии</div>' .
+        '<div id="mw-content-text" lang="ru" dir="ltr" class="mw-content-ltr">' .
+        $this->cutFooter($content) .
+        "</div></div>";
     }
 }
