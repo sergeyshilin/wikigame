40eb555dcffdfe84826d4b9db192fe122cd3aa83
Vladimir Ivashkin <illusionww@gmail.com>
2015-04-07 23:06:40 +0300

    + sources

--- w/classes/PageResolver.php
+++ w/classes/PageResolver.php
@@ -6,7 +6,8 @@ class PageResolver {
     private static $metaSections = array(
         ".D0.9F.D1.80.D0.B8.D0.BC.D0.B5.D1.87.D0.B0.D0.BD.D0.B8.D1.8F", // Примечания
         ".D0.9B.D0.B8.D1.82.D0.B5.D1.80.D0.B0.D1.82.D1.83.D1.80.D0.B0", // Литература
-        ".D0.A1.D1.81.D1.8B.D0.BB.D0.BA.D0.B8"); // Ссылки
+        ".D0.A1.D1.81.D1.8B.D0.BB.D0.BA.D0.B8", // Ссылки
+        ".D0.98.D1.81.D1.82.D0.BE.D1.87.D0.BD.D0.B8.D0.BA.D0.B8"); // Источники
 
     public function getContentFromApi($name) {
         $url = "https://" . $_SESSION['lang'] . ".wikipedia.org/w/api.php?action=parse&page=" . urlencode(urldecode($name)) . "&format=json";
