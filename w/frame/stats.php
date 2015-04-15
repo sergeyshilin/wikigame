a419e39dd807c9a1351afa222f4f633be83ad52d
Sergey Shilin <sergey.shilin@phystech.edu>
2015-04-15 23:38:20 +0300

    fontawesome icons added

--- w/frame/stats.php
+++ w/frame/stats.php
@@ -2,7 +2,7 @@
   <!-- Carousel items -->
   <div class="carousel-inner">
     <div class="active item">
-      <div class="list-group">
+      <div class="list-group" id="popular-today">
         <a class="list-group-item active">
           <span class="badge">Понравилось</span>
           <h4 class="list-group-item-heading">Популярное сегодня</h4>
@@ -10,27 +10,37 @@
         <a href="/fc87692" class="list-group-item">
           <span class="badge">56</span>
           <h4 class="list-group-item-heading">Планеметрия</h4>
-          <p class="list-group-item-text">Гомеоморфизм</p>
+          <p class="list-group-item-text">
+            <i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i> 
+            Гомеоморфизм</p>
         </a>
         <a href="/fb65962" class="list-group-item">
           <span class="badge">45</span>
           <h4 class="list-group-item-heading">Насекомые</h4>
-          <p class="list-group-item-text">Белая армия</p>
+          <p class="list-group-item-text">
+            <i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i> 
+            Белая армия</p>
         </a>
         <a href="/d4f8b48" class="list-group-item">
           <span class="badge">38</span>
           <h4 class="list-group-item-heading">Перигелий</h4>
-          <p class="list-group-item-text">Градус Фаренгейта</p>
+          <p class="list-group-item-text">
+            <i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i> 
+            Градус Фаренгейта</p>
         </a>
         <a href="/2b1ac49" class="list-group-item">
           <span class="badge">33</span>
           <h4 class="list-group-item-heading">Черная дыра</h4>
-          <p class="list-group-item-text">Прокрастинация</p>
+          <p class="list-group-item-text">
+            <i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i> 
+            Прокрастинация</p>
         </a>
         <a href="/c03daa8" class="list-group-item">
           <span class="badge">27</span>
           <h4 class="list-group-item-heading">Обыкновенный бегемот</h4>
-          <p class="list-group-item-text">Ботинки Буша</p>
+          <p class="list-group-item-text">
+            <i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i> 
+            Ботинки Буша</p>
         </a>
         <a class="list-group-item">
           <h4 class="list-group-item-heading"></h4>
@@ -42,7 +52,7 @@
       </div>
     </div>
     <div class="item">
-      <div class="list-group">
+      <div class="list-group" id="all-popular">
         <a class="list-group-item active">
           <span class="badge">Понравилось</span>
           <h4 class="list-group-item-heading">Популярное</h4>
@@ -50,27 +60,37 @@
         <a href="/fc87692" class="list-group-item">
           <span class="badge">56</span>
           <h4 class="list-group-item-heading">Планеметрия</h4>
-          <p class="list-group-item-text">Гомеоморфизм</p>
+          <p class="list-group-item-text">
+            <i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i> 
+            Гомеоморфизм</p>
         </a>
         <a href="/fb65962" class="list-group-item">
           <span class="badge">45</span>
           <h4 class="list-group-item-heading">Насекомые</h4>
-          <p class="list-group-item-text">Белая армия</p>
+          <p class="list-group-item-text">
+            <i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i> 
+            Белая армия</p>
         </a>
         <a href="/d4f8b48" class="list-group-item">
           <span class="badge">38</span>
           <h4 class="list-group-item-heading">Перигелий</h4>
-          <p class="list-group-item-text">Градус Фаренгейта</p>
+          <p class="list-group-item-text">
+            <i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i> 
+            Градус Фаренгейта</p>
         </a>
         <a href="/2b1ac49" class="list-group-item">
           <span class="badge">33</span>
           <h4 class="list-group-item-heading">Черная дыра</h4>
-          <p class="list-group-item-text">Прокрастинация</p>
+          <p class="list-group-item-text">
+            <i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i> 
+            Прокрастинация</p>
         </a>
         <a href="/c03daa8" class="list-group-item">
           <span class="badge">27</span>
           <h4 class="list-group-item-heading">Обыкновенный бегемот</h4>
-          <p class="list-group-item-text">Ботинки Буша</p>
+          <p class="list-group-item-text">
+            <i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i> 
+            Ботинки Буша</p>
         </a>
         <a class="list-group-item">
           <h4 class="list-group-item-heading"></h4>
@@ -82,34 +102,34 @@
       </div>
     </div>
     <div class="item">
-      <div class="list-group">
+      <div class="list-group" id="top-users">
         <a class="list-group-item active">
           <span class="badge">Рейтинг</span>
           <h4 class="list-group-item-heading">Лучшие игроки</h4>
         </a>
         <a class="list-group-item">
           <span class="badge">15450</span>
-          <h4 class="list-group-item-heading">Wiki_Master</h4>
+          <h4 class="list-group-item-heading"><i class="fa fa-user"></i> Wiki_Master</h4>
           <p class="list-group-item-text">Сыграно: 250 игр</p>
         </a>
         <a class="list-group-item">
           <span class="badge">13280</span>
-          <h4 class="list-group-item-heading">ТоварисчОдмин</h4>
+          <h4 class="list-group-item-heading"><i class="fa fa-user"></i> ТоварисчОдмин</h4>
           <p class="list-group-item-text">Сыграно: 292 игры</p>
         </a>
         <a class="list-group-item">
           <span class="badge">10200</span>
-          <h4 class="list-group-item-heading">superwalker</h4>
+          <h4 class="list-group-item-heading"><i class="fa fa-user"></i> superwalker</h4>
           <p class="list-group-item-text">Сыграно: 237 игр</p>
         </a>
         <a class="list-group-item">
           <span class="badge">9950</span>
-          <h4 class="list-group-item-heading">hackwiki</h4>
+          <h4 class="list-group-item-heading"><i class="fa fa-user"></i> hackwiki</h4>
           <p class="list-group-item-text">Сыграно: 194 игры</p>
         </a>
         <a class="list-group-item">
           <span class="badge">9800</span>
-          <h4 class="list-group-item-heading">ueba</h4>
+          <h4 class="list-group-item-heading"><i class="fa fa-user"></i> ueba</h4>
           <p class="list-group-item-text">Сыграно: 201 игра</p>
         </a>
         <a class="list-group-item">
